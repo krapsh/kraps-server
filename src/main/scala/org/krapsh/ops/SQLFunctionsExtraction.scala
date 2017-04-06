e0923e4d8dda2b5f5c34ae9b8f03487a035d7bce
krapsh <krapsh@yandex.com>
2017-04-06 16:34:27 -0700

    [31] Fix for broadcastPair (#12)
    * fix for broadcast pair
    
    * cleanup

--- src/main/scala/org/krapsh/ops/SQLFunctionsExtraction.scala
+++ src/main/scala/org/krapsh/ops/SQLFunctionsExtraction.scala
@@ -3,10 +3,11 @@ package org.krapsh.ops
 import com.typesafe.scalalogging.slf4j.{StrictLogging => Logging}
 import org.apache.spark.sql.{Column, KrapshStubs, _}
 import org.apache.spark.sql.catalyst.analysis.FunctionRegistry
+import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.catalyst.expressions.aggregate.AggregateFunction
 import org.apache.spark.sql.types.{IntegerType, LongType}
-import org.krapsh.ColumnWithType
-import org.krapsh.structures.{AugmentedDataType, IsStrict}
+import org.krapsh.{ColumnWithType, KrapshException}
+import org.krapsh.structures.{AugmentedDataType, IsNullable, IsStrict, Nullable}
 
 import scala.util.{Failure, Success, Try}
 
@@ -14,7 +15,52 @@ import scala.util.{Failure, Success, Try}
 object SQLFunctionsExtraction extends Logging {
   type SQLFunctionName = String
 
-  def build(n: SQLFunctionName, inputs: Seq[ColumnWithType]): Try[ColumnWithType] = {
+  def buildFunction(
+      funName: String,
+      inputs: Seq[ColumnWithType],
+      ref: DataFrame): Try[ColumnWithType] = {
+    FunctionRegistry.builtin.lookupFunctionBuilder(funName.toLowerCase) match {
+      case Some(builder) =>
+        val exps = inputs.map(_.col).map(KrapshStubs.getExpression)
+        val expt = Try {
+          builder.apply(exps)
+        }
+        expt.map { exp =>
+          logger.debug(s"buildFunction: exp=$exp")
+          val c = exp match {
+            case agg: AggregateFunction =>
+              KrapshStubs.makeColumn(agg.toAggregateExpression(isDistinct = false))
+            case x =>
+              KrapshStubs.makeColumn(x)
+          }
+          // As an approximation, the types are going to be the ones from Spark.
+          // We may loose some information here, but it should be good enough as a start.
+          // In order to determine the nullability, we need to apply the expression first.
+
+          val adt = checkNullability(c, ref)
+          val cwt = ColumnWithType(c, adt, ref)
+          logger.debug(s"buildFunction: cwt=$cwt")
+          // The output type may not be supported by Karps, so it needs to be rectified.
+          val rectified = rectifyNumericalType(cwt)
+          logger.debug(s"buildFunction: rectified=$rectified")
+          rectified
+        }
+
+      case None => Failure(new KrapshException(s"Could not find function name '$funName' in the " +
+        s"spark registry"))
+    }
+  }
+
+  private def checkNullability(c: Column, df: DataFrame): AugmentedDataType = {
+    val df2 = df.select(c)
+    df2.schema.fields match {
+      case Array(f1) =>
+        AugmentedDataType.fromField(f1)
+      case _ => KrapshException.fail(s"df=$df df2=$df2 c=$c")
+    }
+  }
+
+  private def build(n: SQLFunctionName, inputs: Seq[ColumnWithType]): Try[ColumnWithType] = {
     logger.info(s"build called with n=$n inputs=$inputs")
     if (inputs.isEmpty) {
       // TODO: fix eventually
@@ -45,7 +91,21 @@ object SQLFunctionsExtraction extends Logging {
     }
   }
 
+  // This is very simple and does not attempt to be recursive.
+  // TODO: make it recursive
+  // TODO: prevent it from creating bad types to begin with.
+  private def rectifyNumericalType(cwt: ColumnWithType): ColumnWithType = {
+    cwt.rectifiedSchema.dataType match {
+      case x: LongType =>
+        val adt = cwt.rectifiedSchema.copy(dataType = IntegerType)
+        ColumnWithType(cwt.col.cast(IntegerType), adt, cwt.ref)
+      case _ => cwt
+    }
+  }
+
   private def rectifyNumericalType(inputs: Seq[ColumnWithType], c: Column): ColumnWithType = {
+    logger.debug(s"rectifyNumericalType: inputs=$inputs")
+    logger.debug(s"rectifyNumericalType: c=$c")
     // TODO: unsafe
     val ref = inputs.head.ref
     def isPrimNum(col: ColumnWithType): Option[AugmentedDataType] = {
@@ -65,11 +125,13 @@ object SQLFunctionsExtraction extends Logging {
     // For the purpose of matching, since Spark makes some mistakes, we assume it is strict for
     // numerical stuff.
     val o = isPrimNum(ColumnWithType(c, AugmentedDataType(sadt.dataType, IsStrict), ref))
-    (i, o) match {
+    val res = (i, o) match {
       case (Some(it), Some(ot)) if it != ot =>
         ColumnWithType(c.cast(it.dataType), it, ref)
       case _ =>
         ColumnWithType(c, sadt, ref)
     }
+    logger.debug(s"rectifyNumericalType: res=$res")
+    res
   }
 }
